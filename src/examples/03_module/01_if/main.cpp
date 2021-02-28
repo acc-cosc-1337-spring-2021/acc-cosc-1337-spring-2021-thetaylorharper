a7b080adc6a7b78a51f82711a8f064a0215d7564
Taylor Harper <taylor.harper2@g.austincc.edu>
2021-02-27 18:34:20 -0600

    if, else_if, switch

--- src/examples/03_module/01_if/main.cpp
+++ src/examples/03_module/01_if/main.cpp
@@ -7,16 +7,24 @@ using std::cin;
 int main() 
 {
 	//create a bool variable named even
+	bool even;
 	//create an int variable named value
-
+	int value;
 	//prompt user for a keyboard number
+	cout<<"enter a number: ";
 	//assign the number to the value variable
-
+	cin>>value;
 	//call the is_even function with value as its argument
+	even = is_even(value);
 	//and assign its return value to the even variable
 	
 
 	//display even if number is even
+	if(even == true)
+	{
+		cout<<value<<" is even";
+	}
+
 
 
 	return 0;
