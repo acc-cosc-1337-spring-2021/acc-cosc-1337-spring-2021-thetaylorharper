2f1fd2a6654fde12a41a1ec44363367231b32e8a
Taylor Harper <taylor.harper2@g.austincc.edu>
2021-02-28 19:17:48 -0600

    homework 3 converting numerical grade

--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -1,10 +1,16 @@
 //Write the include statement for decisions.h here
-
+#include "decision.h"
+#include <iostream>
 //Write namespace using statements for cout and cin
-
+using std::cin;	using std::cout;
 int main() 
-{
+{	auto grade = 100;
+	cout<<"Enter your numerical grade: ";
+	cin>>grade;
 
+	cout<<"Grade Recieved: "<<get_letter_grade_using_if(grade)<<"\n";
+	cout<<"Grade Recieved: "<<get_letter_grade_using_switch(grade)<<"\n";
+	
 	return 0;
 }
 
