3a17f5b95c38059b1ea8232b57886d8ebc341ab5
Taylor Harper <taylor.harper2@g.austincc.edu>
2021-03-10 15:53:24 -0600

    homework 4 loops

--- src/classwork/04_assign/loops.cpp
+++ src/classwork/04_assign/loops.cpp
@@ -1,15 +1,19 @@
 //cpp
+#include "loops.h"
 
+int sum = 1;
+int fact = 1;
+int factorial(int num)
+{
+    
+    while(fact <= num)
+    {
+        sum = sum * fact;
+        fact++;
+        
+    }
 
+    return sum;
+    
+}
 
-Understanding the Problem:
-A factorial is a number's product and all numbers preceding.  
-Example factorial of 5 is  1*2*3*4*5 = 120.
-Implementation:
-Open your acc-cosc-1337-fall-2020-username project.
-WRITE CODE 
-1) To begin your assignment, find the src/classwork/04_assign folder.  Under this folder:
-      a) In file src/classwork/04_assign/loops.h, write function prototype for a value-return function named factorial that accepts
-     an int parameter named num and  returns an int. 
-      b) In file src/classwork/04_assign/loops.cpp, write the function code that returns the factorial of num using 
-          a loop of your choice.  Do not use a recursive solution to solve this problem.
--- test/classwork_test/04_assign_test/loops_tests.cpp
+++ test/classwork_test/04_assign_test/loops_tests.cpp
@@ -6,3 +6,11 @@ TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Test factorial loop", "return the factorial of any given number") 
+{
+	REQUIRE(factorial(3) == 6);
+	REQUIRE(factorial(4) == 24);
+	REQUIRE(factorial(5) == 120);
+
+}
+
