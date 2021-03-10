a68c4448d8d6a0ae63af941ba50a438d6a2db5bf
Taylor Harper <taylor.harper2@g.austincc.edu>
2021-03-09 21:27:03 -0600

    ex for ranged main

--- src/examples/04_module/04_for_ranged/main.cpp
+++ src/examples/04_module/04_for_ranged/main.cpp
@@ -1,9 +1,11 @@
-#include<string>
-#include<iostream>
+
 #include "for_ranged.h"
 
 int main() 
 {
+	std::string name = "test";
 
+	loop_string_w_for_loop(name); //passing in string as a parameter
+	loop_string_w_for_range(name);
 	return 0;
 }
