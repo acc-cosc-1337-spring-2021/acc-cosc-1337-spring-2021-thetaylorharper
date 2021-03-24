395b0de4c5f9813f28a74ec104f63ad2795e14d7
Taylor Harper <taylor.harper2@g.austincc.edu>
2021-03-24 17:00:05 -0500

    vectors examples for ranged value & ref paramters

--- src/examples/04_module/07_vectors_for_ranged/vec_for_ranged.cpp
+++ src/examples/04_module/07_vectors_for_ranged/vec_for_ranged.cpp
@@ -3,6 +3,7 @@
 #include<vector>
 
 using std::vector;
+using std::cout;
 
 /*
 Function loop_vector_w_for_ranged
@@ -20,7 +21,21 @@ Displays the numbers to screen using.
 
 @param vector<int>
 */
+void loop_vector_w_for_ranged_ref(std::vector<int>& nums)
+{
+    for(auto num: nums)
+    {
+        cout<<num<<"\n";
+        num = -1;
+    }
 
+    cout<<"\n\n vector results\n";
+
+    for(auto num: nums)
+    {
+        cout<<num<<"\n";
+    }
+}
 
 
 /*
@@ -30,7 +45,21 @@ Displays the numbers to screen
 
 @param vector<int>&
 */
+void loop_vector_w_for_ranged_ref(std::vector<int>& nums)
+{
+    for(auto& num: nums)
+    {
+        cout<<num<<"\n";
+        num = -1;
+    }
 
+    cout<<"\n\n REF vector results\n";
+
+    for(auto num: nums)
+    {
+        cout<<num<<"\n";
+    }
+}
 
 /*
 Function loop_vector_w_for_ranged_const_ref
@@ -39,4 +68,13 @@ Displays the numbers to screen
 
 @param const vector<int>&
 */
+vector<int> get_vector()
+{
+    vector<int> nums;
+    nums.push_back(5);
+    nums.push_back(1);
+    nums.push_back(10); //5, 1, 10
 
+    return nums;
+
+}
--- test/examples_test/04_module_test/04_module_tests.cpp
+++ test/examples_test/04_module_test/04_module_tests.cpp
@@ -3,8 +3,10 @@
 #include "while.h"
 #include "value_ref.h"
 #include "for_ranged.h"
+#include "vec.h"
 
 using std::string;
+using std::vector;
 
 
 
@@ -63,4 +65,46 @@ TEST_CASE("Test string for ranged by reference param")
 	loop_string_w_for_range_ref(name);
 
 	REQUIRE(name == "zzzz");
+}
+
+TEST_CASE("Test vector as value paramter")
+{
+	vector<int> expected_values{10, 3, 2};
+	vector<int> nums{10, 3, 2};//create and assign nums
+
+	loop_vector_w_index_val(nums);
+
+	REQUIRE(nums == expected_values);
+
+}
+
+TEST_CASE("Test vector as reference paramater")
+{
+	vector<int> expected_values{-1, -1, -1};
+	vector<int> nums{10, 3, 2};//create and assign nums
+
+	loop_vector_w_index_ref(nums);
+
+	REQUIRE(nums == expected_values);
+
+}
+
+TEST_CASE("Test vector for ranged value paramter")
+{
+	vector<int> expected_values{10, 3, 2};
+	vector<int> nums{10, 3, 2};//create and assign nums
+
+	loop_vector_w_for_ranged(nums);
+
+	REQUIRE(nums == expected_values);
+}
+
+TEST_CASE("Test vector fo ranged reference paramater")
+{
+	vector<int> expected_values{10, 3, 2};
+	vector<int> nums{10, 3, 2};//create and assign nums
+
+	loop_vector_w_for_ranged_ref(nums);
+
+	REQUIRE(nums == expected_values);
 }
