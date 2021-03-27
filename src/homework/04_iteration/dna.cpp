17cce184c684cbc724465bce42a19786650c1a23
Taylor Harper <taylor.harper2@g.austincc.edu>
2021-03-27 16:36:54 -0500

    Homework 05 Dna

--- src/homework/04_iteration/dna.cpp
+++ src/homework/04_iteration/dna.cpp
@@ -1,4 +1,8 @@
 #include "dna.h"
+#include<iostream>
+#include<cmath>
+using std::string;
+using std::cout;
 /*
 Write code for function get_gc_content that accepts
 a const reference string parameter and returns a double.
@@ -6,6 +10,22 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
+double get_gc_content(const string& dna)
+{
+    double gc_content;
+    double diff = 0.0;
+    for (size_t i = 0; i < dna.size(); i++)
+    {
+        if (dna[i] == 'G' || dna[i] == 'C')
+        diff = diff + 1;
+
+    }
+    gc_content = diff / dna.size();
+    return gc_content;
+}
+
+
+
 
 
 
@@ -16,6 +36,16 @@ accepts a string parameter and returns a string reversed.
 */
 
 
+string reverse_string(string dna)
+{
+   string reverse_dna;
+   for (int i = dna.size() - 1; i >= 0; i--)//Can not use the unsigned int size_t with out error?
+   {
+       reverse_dna.push_back(dna[i]);
+   }
+    
+    return reverse_dna;
+}
 
 /*
 Write prototype for function get_dna_complement that
@@ -28,3 +58,25 @@ c. return string
 
 */
 
+
+std::string get_dna_complement(string dna)
+{
+    string to_be_complement = reverse_string(dna);
+    string dna_complement;
+    for(auto s: to_be_complement)
+    {
+        if (s == 'A')
+        dna_complement.push_back('T');
+        else if(s == 'T')
+        dna_complement.push_back('A');
+        else if(s == 'G')
+        dna_complement.push_back('C');
+        else if(s == 'C')
+        dna_complement.push_back('G');
+    }
+    
+    return dna_complement;
+}
+    
+
+    
--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -1,4 +1,8 @@
 //write include statements
+#include "dna.h"
+#include<iostream>
+using std::string;
+using std::cout;    using std::cin;
 
 //write using statements
 
@@ -12,5 +16,34 @@ user enters a y or Y.
 */
 int main() 
 {
-	return 0;
+    char choice;
+    do
+    {
+        cout<<"Enter 1 for GC Conten. Enter 2 for DNA Complement. Press Y to exit: ";
+        cin>>choice;
+        string dna;
+        if (choice == '1')
+        {   
+            cout<<"Please enter DNA string: ";
+            cin>>dna;
+            cout<<get_gc_content(dna)<<"\n";
+        }
+        else if(choice == '2')
+        {
+            cout<<"Please enter DNA string: ";
+            cin>>dna;
+            cout<<get_dna_complement(dna)<<"\n";
+        }
+        else if (choice != '1' && choice !='2' && choice != 'y' && choice != 'Y')
+        cout<<"Please enter a valid entry. \n";
+    }
+    while ((choice != 'y' && choice !='Y'));
+        
+        
+
+
+    return 0;
+    
+
+     
 }
