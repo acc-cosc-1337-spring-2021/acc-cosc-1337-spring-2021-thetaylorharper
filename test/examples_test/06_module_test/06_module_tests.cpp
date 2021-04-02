7beb970c7ec70334c7baa956b4dbeeffde1eadcd
Taylor Harper <taylor.harper2@g.austincc.edu>
2021-04-02 17:40:26 -0500

    Module 6 test: Bank Account

--- test/examples_test/06_module_test/06_module_tests.cpp
+++ test/examples_test/06_module_test/06_module_tests.cpp
@@ -1,7 +1,106 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "bank_account.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Test new bank account balance is 0")
+{
+	BankAccount account;
+	REQUIRE(account.get_balance() == 0);
+}
+
+TEST_CASE("Test new bank account with given begin balance")
+{
+	BankAccount account(100);
+	REQUIRE(account.get_balance() == 100);
+}
+
+TEST_CASE("Test bank account depoit with valid ammount")
+{
+	BankAccount account;
+	REQUIRE(account.get_balance() == 0);
+	
+	account.deposit(50);
+	REQUIRE(account.get_balance() == 50);
+
+}
+
+TEST_CASE("Test account withdraw with given begin balance")
+{
+	BankAccount account(100);
+	account.withdraw(10);
+	REQUIRE(account.get_balance() == 90);
+}
+
+
+
+TEST_CASE("Test bank account deposit with given begin balance")
+{
+	BankAccount account(100);
+
+	account.deposit(50);
+	REQUIRE(account.get_balance() == 150);
+
+}
+
+TEST_CASE("Test bank account depoit with invalid ammount")
+{
+	BankAccount account;
+	REQUIRE(account.get_balance() == 0);
+	
+	account.deposit(50);
+	REQUIRE(account.get_balance() == 50);
+
+	account.deposit(-10);
+	REQUIRE(account.get_balance() == 50);
+
+}
+
+TEST_CASE("Test bank account withdraw with valid ammount")
+{
+	BankAccount account;
+	REQUIRE(account.get_balance() == 0);
+	
+	account.deposit(50);
+	REQUIRE(account.get_balance() == 50);
+
+	account.deposit(-10);
+	REQUIRE(account.get_balance() == 50);
+
+	account.withdraw(10);
+	REQUIRE(account.get_balance() == 40);
+}
+
+TEST_CASE("Test bank account withdraw with invalid ammount ")
+{
+	BankAccount account;
+	REQUIRE(account.get_balance() == 0);
+	
+	account.deposit(50);
+	REQUIRE(account.get_balance() == 50);
+
+	account.deposit(-10);
+	REQUIRE(account.get_balance() == 50);
+
+	account.withdraw(10);
+	REQUIRE(account.get_balance() == 40);
+
+	account.withdraw(-10);
+	REQUIRE(account.get_balance() == 40);
+}
+
+TEST_CASE("Test bank account withdraw with ammount greater than balance")
+{
+	BankAccount account;
+	REQUIRE(account.get_balance() == 0);
+	
+	account.deposit(50);
+	REQUIRE(account.get_balance() == 50);
+
+	account.withdraw(100);
+	REQUIRE(account.get_balance() == 50);
 }
 
